091187da170afd09d6d1ad885ae86962e604ae7a
Wajih-Georges El Tayar <wgtayar@gmail.com>
2025-05-13 01:22:19 +0300

    Working version of the BT (the less fancy one).

--- src/warde_bt/src/warde_bt_main.cpp
+++ src/warde_bt/src/warde_bt_main.cpp
@@ -1,11 +1,6 @@
-#include <memory>
-#include <string>
-#include <chrono>
-
 #include <rclcpp/rclcpp.hpp>
 #include <ament_index_cpp/get_package_share_directory.hpp>
 #include <behaviortree_cpp_v3/bt_factory.h>
-
 #include <behaviortree_cpp_v3/decorators/repeat_node.h>
 #include <behaviortree_cpp_v3/controls/reactive_sequence.h>
 #include <behaviortree_cpp_v3/controls/reactive_fallback.h>
@@ -15,9 +10,6 @@
 #include "warde_bt/action_get_closest_beer.h"
 #include "warde_bt/action_navigate.h"
 #include "warde_bt/action_manipulate.h"
-#include "robot_nav/srv/navigate.hpp"
-
-using namespace std::chrono_literals;
 
 int main(int argc, char **argv)
 {
@@ -26,21 +18,10 @@ int main(int argc, char **argv)
     rclcpp::executors::SingleThreadedExecutor exec;
     exec.add_node(node);
 
-    using NavigateReq = robot_nav::srv::Navigate::Request;
-    using NavigateRes = robot_nav::srv::Navigate::Response;
-    node->create_service<robot_nav::srv::Navigate>(
-        "navigate",
-        [&](std::shared_ptr<NavigateReq>, std::shared_ptr<NavigateRes> res)
-        {
-            res->success = true;
-            res->message = "ok";
-        });
-
-    auto bb = BT::Blackboard::create();
-    bb->set("ros_node", node);
+    auto blackboard = BT::Blackboard::create();
+    blackboard->set("node", node);
 
     BT::BehaviorTreeFactory factory;
-
     factory.registerNodeType<BT::RepeatNode>("DecoratorLoop");
     factory.registerNodeType<BT::ReactiveFallback>("FallbackNoMemory");
     factory.registerNodeType<warde_bt::ConditionBoxPresent>("ConditionBoxPresent");
@@ -49,19 +30,16 @@ int main(int argc, char **argv)
     factory.registerNodeType<warde_bt::ActionNavigate>("ActionNavigate");
     factory.registerNodeType<warde_bt::ActionManipulate>("ActionManipulate");
 
-    auto pkg_share = ament_index_cpp::get_package_share_directory("warde_bt");
-    auto xml_file = pkg_share + "/config/warde_tree.xml";
-    RCLCPP_INFO(node->get_logger(), "Loading BT from: %s", xml_file.c_str());
+    auto xml = ament_index_cpp::get_package_share_directory("warde_bt") + "/config/warde_tree.xml";
+    auto tree = factory.createTreeFromFile(xml, blackboard);
 
-    factory.registerBehaviorTreeFromFile(xml_file);
-    auto tree = factory.createTree("MainTree", bb);
-
-    rclcpp::Rate loop_rate(10);
-    while (rclcpp::ok())
+    BT::NodeStatus status = BT::NodeStatus::RUNNING;
+    rclcpp::Rate rate(10);
+    while (rclcpp::ok() && status == BT::NodeStatus::RUNNING)
     {
-        tree.rootNode()->executeTick();
+        status = tree.rootNode()->executeTick();
         exec.spin_some();
-        loop_rate.sleep();
+        rate.sleep();
     }
 
     rclcpp::shutdown();
