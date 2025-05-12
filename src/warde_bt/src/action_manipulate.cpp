f37f4c944ee52c922f1488df470e5e9d87deb398
Wajih-Georges El Tayar <wgtayar@gmail.com>
2025-05-12 09:22:34 +0300

    Commented out the manipulation action for testing purposes.

--- src/warde_bt/src/action_manipulate.cpp
+++ src/warde_bt/src/action_manipulate.cpp
@@ -1,74 +1,78 @@
 #include "warde_bt/action_manipulate.h"
 
-using namespace std::chrono_literals;
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// THIS IS A DUMMY IMPLEMENTATION OF THE ACTION MANIPULATE CLASS, WAITING FOR THE ACTUAL IMPLEMENTATION TO BE READY.
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-namespace warde_bt
-{
+// using namespace std::chrono_literals;
 
-    ActionManipulate::ActionManipulate(
-        const std::string &name,
-        const BT::NodeConfiguration &config)
-        : BT::SyncActionNode(name, config)
-    {
-        node_ = rclcpp::Node::make_shared("bt_manipulate_client");
-        client_ = node_->create_client<manipulation_msgs::srv::Manipulate>("manipulation_service");
-    }
+// namespace warde_bt
+// {
 
-    BT::NodeStatus ActionManipulate::tick()
-    {
-        std::string manipulation, pre_frame, target_frame;
-        if (!getInput("manipulation", manipulation))
-        {
-            throw BT::RuntimeError("ActionManipulate missing [manipulation] port");
-        }
-        if (!getInput("pre_target_frame", pre_frame))
-        {
-            throw BT::RuntimeError("ActionManipulate missing [pre_target_frame] port");
-        }
-        if (!getInput("target_frame", target_frame))
-        {
-            throw BT::RuntimeError("ActionManipulate missing [target_frame] port");
-        }
+//     ActionManipulate::ActionManipulate(
+//         const std::string &name,
+//         const BT::NodeConfiguration &config)
+//         : BT::SyncActionNode(name, config)
+//     {
+//         node_ = rclcpp::Node::make_shared("bt_manipulate_client");
+//         client_ = node_->create_client<manipulation_msgs::srv::Manipulate>("manipulation_service");
+//     }
 
-        if (!client_->wait_for_service(2s))
-        {
-            RCLCPP_ERROR(node_->get_logger(), "manipulation_service not available");
-            return BT::NodeStatus::FAILURE;
-        }
+//     BT::NodeStatus ActionManipulate::tick()
+//     {
+//         std::string manipulation, pre_frame, target_frame;
+//         if (!getInput("manipulation", manipulation))
+//         {
+//             throw BT::RuntimeError("ActionManipulate missing [manipulation] port");
+//         }
+//         if (!getInput("pre_target_frame", pre_frame))
+//         {
+//             throw BT::RuntimeError("ActionManipulate missing [pre_target_frame] port");
+//         }
+//         if (!getInput("target_frame", target_frame))
+//         {
+//             throw BT::RuntimeError("ActionManipulate missing [target_frame] port");
+//         }
 
-        auto req = std::make_shared<manipulation_msgs::srv::Manipulate::Request>();
-        req->manipulation = manipulation;
-        req->pre_target_frame = pre_frame;
-        req->target_frame = target_frame;
+//         if (!client_->wait_for_service(2s))
+//         {
+//             RCLCPP_ERROR(node_->get_logger(), "manipulation_service not available");
+//             return BT::NodeStatus::FAILURE;
+//         }
 
-        auto future = client_->async_send_request(req);
+//         auto req = std::make_shared<manipulation_msgs::srv::Manipulate::Request>();
+//         req->manipulation = manipulation;
+//         req->pre_target_frame = pre_frame;
+//         req->target_frame = target_frame;
 
-        if (rclcpp::spin_until_future_complete(node_, future) != rclcpp::FutureReturnCode::SUCCESS)
-        {
-            RCLCPP_ERROR(node_->get_logger(), "manipulation_service call failed");
-            return BT::NodeStatus::FAILURE;
-        }
+//         auto future = client_->async_send_request(req);
 
-        auto res = future.get();
-        if (!res->success)
-        {
-            RCLCPP_ERROR(
-                node_->get_logger(),
-                "manipulation failed: %s",
-                res->message.c_str());
-            return BT::NodeStatus::FAILURE;
-        }
+//         if (rclcpp::spin_until_future_complete(node_, future) != rclcpp::FutureReturnCode::SUCCESS)
+//         {
+//             RCLCPP_ERROR(node_->get_logger(), "manipulation_service call failed");
+//             return BT::NodeStatus::FAILURE;
+//         }
 
-        RCLCPP_INFO(
-            node_->get_logger(),
-            "manipulation succeeded: %s",
-            res->message.c_str());
-        return BT::NodeStatus::SUCCESS;
-    }
+//         auto res = future.get();
+//         if (!res->success)
+//         {
+//             RCLCPP_ERROR(
+//                 node_->get_logger(),
+//                 "manipulation failed: %s",
+//                 res->message.c_str());
+//             return BT::NodeStatus::FAILURE;
+//         }
+
+//         RCLCPP_INFO(
+//             node_->get_logger(),
+//             "manipulation succeeded: %s",
+//             res->message.c_str());
+//         return BT::NodeStatus::SUCCESS;
+//     }
 
     // BT_REGISTER_NODES(factory)
     // {
     //     factory.registerNodeType<ActionManipulate>("ActionManipulate");
     // }
 
-}
+// }
